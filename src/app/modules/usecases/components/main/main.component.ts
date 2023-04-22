fdae87f98e33a0d261f5c9e222cf4024ccd60e51
Lujandev <eddylujann@gmailc.om>
2023-04-22 23:49:08 +0200

    up

--- src/app/modules/usecases/components/main/main.component.ts
+++ src/app/modules/usecases/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ScriptsService } from '@data/services/api/scripts.service';
 import { fromEvent, Observable, Subscription } from 'rxjs';
@@ -19,20 +19,38 @@ export class MainComponent implements OnInit {
     small: 0,
     medium: 400,
     large: 800,
-};
+  };
+
+  @ViewChild('rx9f619') rx9f619: ElementRef;
 
   constructor(
     public sanitizer: DomSanitizer,
     public scripts: ScriptsService,
+    private render2: Renderer2
   ) {
     //scripts.loadFiles(['responsive']);
     //this.cssUrl = '/assets/css/responsive.css';
     //this.cssUrl = '/assets/css/root.css';
   }
 
+  ngAfterViewInit() {
+    const rx9f619 = this.rx9f619.nativeElement;
+    this.render2.setStyle(rx9f619, 'color', 'red');
+  }
+
   ngOnInit() {
-    //this.screenWidth = window.innerWidth;  
-    //console.log(this.screenWidth);
+    this.setupView();
+  }
+
+  setupView() {
+    this.resizeWindows();
+  }
+
+  /**
+   * Detectar la redimenzion de la pantalla.
+   * con un clear timeout.
+   */
+  private resizeWindows() {
     this.resizeObservable$ = fromEvent(window, 'resize');
     this.resizeSubscription$ = this.resizeObservable$.subscribe(e => {
       clearTimeout(this.resizeId);
@@ -41,7 +59,6 @@ export class MainComponent implements OnInit {
       }, 300);
     });
   }
-
 
   /**
    * @description Controlador de cambio de tamaño del 
@@ -58,33 +75,62 @@ export class MainComponent implements OnInit {
     for(let s in this.screen) {
       if (screenWidth >= this.screen[s]) size = s; iw = screenWidth;
     }
-    console.log("size=> " + size + " and " + "new => " + iw);
     // Aplicar la media Query en cada tamaño de la pantalla.
-    this.mediaQuery(iw!);
-
+    this.mediaQuery(size!, iw!);
   }
 
-  public mediaQuery( iw:number ) {
-    /*if (window.innerWidth <= 400) {
-      this.renderMobileView();
-    } else {
-      this.renderDesktopView();
-    }*/
+  /**
+   * Opciones de mediaQuery para diferentes anchos de la pantalla.
+   * @param size 
+   * @param iw 
+   */
+  public mediaQuery( size: string, iw:number ) {
+    switch ( size ) {
+      case 'small':
+        this.renderMobileView( iw );
+        break;
+      case 'medium':
+          this.renderTabletView( iw );
+          break;
+      case 'large':
+        this.renderDesktopView( iw );
+        break;
+      default: console.log("error..");
+        break;
+    }
+  }
+
+  /**
+   * Se rederiza para aplicaciones moviles.
+   * @param iw 
+   */
+  renderMobileView( iw:number ) {
+    console.log('render mobile view : ' + iw);
+    this.mobileView = true;
+  }
+
+  /**
+   * Se renderiza para aplicaciones tablet.
+   * @param iw 
+   */
+  renderTabletView( iw:number ) {
+    console.log('render tablet view : ' + iw);
+    if ( iw <= 767 ) {
+      
+    }
+    this.mobileView = true;
+  }
+
+  /**
+   * Se renderiza para aplicaciones desktop.
+   * @param iw 
+   */
+  renderDesktopView( iw:number ) {
+    console.log('render desktop view : ' + iw);
+    this.mobileView = false;
   }
 
   ngOnDestroy() {
     this.resizeSubscription$.unsubscribe();
   }
-
-  renderMobileView() {
-    console.log('render mobile view');
-    this.mobileView = true;
-  }
-
-  renderDesktopView() {
-    console.log('render desktop view');
-    this.mobileView = false;
-  }
-
-
 }
