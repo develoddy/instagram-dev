7070db94979a0dd50927bceb246809c872022209
Eddy Lujan <eddylujann@gmail.com>
2023-04-22 19:47:33 +0200

    up02

--- src/app/modules/usecases/components/main/main.component.ts
+++ src/app/modules/usecases/components/main/main.component.ts
@@ -15,7 +15,11 @@ export class MainComponent implements OnInit {
   resizeSubscription$: Subscription;
   mobileView = false;
   public resizeId: any;
-
+  private screen : any = {
+    small: 0,
+    medium: 400,
+    large: 800,
+};
 
   constructor(
     public sanitizer: DomSanitizer,
@@ -27,31 +31,45 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    //this.screenWidth = window.innerWidth;  
+    //console.log(this.screenWidth);
     this.resizeObservable$ = fromEvent(window, 'resize');
     this.resizeSubscription$ = this.resizeObservable$.subscribe(e => {
       clearTimeout(this.resizeId);
       this.resizeId = setTimeout(() => {
         this.resizeHandler();
       }, 300);
-
-
-
-      
     });
-    //this.screenWidth = window.innerWidth;  
-    //console.log(this.screenWidth);
   }
 
-  public resizeHandler() {
-    console.log("hacer algo...");
 
-    if (window.innerWidth <= 400) {
+  /**
+   * @description Controlador de cambio de tamaño del 
+   * width de la pantalla.
+   * @param
+   * @return
+   */
+  public resizeHandler() {
+    // Obtener ancho de ventana.
+    const screenWidth = window.innerWidth;
+    // Determinar el tamaño y el ancho con nombre.
+    let size = null;
+    let iw = null;
+    for(let s in this.screen) {
+      if (screenWidth >= this.screen[s]) size = s; iw = screenWidth;
+    }
+    console.log("size=> " + size + " and " + "new => " + iw);
+    // Aplicar la media Query en cada tamaño de la pantalla.
+    this.mediaQuery(iw!);
+
+  }
+
+  public mediaQuery( iw:number ) {
+    /*if (window.innerWidth <= 400) {
       this.renderMobileView();
     } else {
       this.renderDesktopView();
-    }
-    
+    }*/
   }
 
   ngOnDestroy() {
