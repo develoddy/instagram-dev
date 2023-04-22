c3d2eb110181ae1941c9786243207092b7a8355f
Lujandev <eddylujann@gmailc.om>
2023-04-22 13:03:53 +0200

    Up

--- src/app/modules/usecases/components/main/main.component.ts
+++ src/app/modules/usecases/components/main/main.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ScriptsService } from '@data/services/api/scripts.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  public cssUrl: string;
+
+  constructor(
+    public sanitizer: DomSanitizer,
+    public scripts: ScriptsService,
+  ) {
+    //scripts.loadFiles(['responsive']);
+    //this.cssUrl = '/assets/css/responsive.css';
+    //this.cssUrl = '/assets/css/root.css';
+    
+  }
+  
+  ngOnInit() {}
+
 
 }
