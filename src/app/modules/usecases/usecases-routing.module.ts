4e0309d65ee3bbbe2c5c4682d814ef3ce9051e11
Eddy Lujan <eddylujann@gmail.com>
2023-04-22 18:17:41 +0200

    nav

--- src/app/modules/usecases/usecases-routing.module.ts
+++ src/app/modules/usecases/usecases-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [{
   path: '', component: MainComponent,
   children: [
     { path: '', redirectTo: "feed", pathMatch: "full" },
+    { path: 'feed', component: FeedComponent},
+    { path: 'search', component: SearchComponent},
   ]
 }];
 
--- src/app/modules/usecases/usecases.module.ts
+++ src/app/modules/usecases/usecases.module.ts
@@ -3,11 +3,13 @@ import { UsecasesRoutingModule } from './usecases-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { MainComponent } from './components/main/main.component';
 import { FeedComponent } from './components/feed/feed.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     MainComponent,
-    FeedComponent
+    FeedComponent,
+    SearchComponent
   ],
   imports: [
     SharedModule,
