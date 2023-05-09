6694b73b3da686bca56a56691cd3b13225bd0539
Eddy Lujan <eddylujann@gmail.com>
2023-05-09 13:23:05 +0200

    Shared post

--- src/app/shared/components/util/modal-sharepost/modal-sharepost.component.ts
+++ src/app/shared/components/util/modal-sharepost/modal-sharepost.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FiltroService } from '@data/services/api/filtro.service';
 
+export interface User {
+  id: number;
+  username: string;
+  checked: boolean
+}
+
 @Component({
   selector: 'app-modal-sharepost',
   templateUrl: './modal-sharepost.component.html',
@@ -10,14 +16,17 @@ export class ModalSharepostComponent implements OnInit {
   
   public isCheck: boolean;
   public isChangedBlock: number;
-  public boxMessage: boolean;
-  @Input() data = [ "1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1" ];
+  public isCheckInputMessage: boolean;
+  public obj_usersSelected: User[] = [];
+  @Input() data = [
+    { profileUrl:"https://images.unsplash.com/photo-1614283233556-f35b0c801ef1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80", username: "rebeca2372", fullname: "Rebeca Marroquín", checked: false, id: 0 },
+    { profileUrl:"https://images.unsplash.com/photo-1614283233556-f35b0c801ef1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80", username: "lujandev", fullname: "Eddy Lujan", checked: false, id: 1 },
+    { profileUrl:"https://images.unsplash.com/photo-1614283233556-f35b0c801ef1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80", username: "paola", fullname: "Paola Lujan", checked: false, id: 2 },
+  ];
 
-  activeIndex: number;
-  
   constructor( public filter: FiltroService) {
-    this.boxMessage = false;
-    this.isCheck = false;
+      this.isCheckInputMessage = false;
+      this.isCheck = true;
   }
 
   ngOnInit() { 
@@ -25,6 +34,35 @@ export class ModalSharepostComponent implements OnInit {
   }
 
   selectUser(index:number ) {
-      this.activeIndex = index;
+      this.data.map( item => { 
+          if ( item.id == index ) { 
+            item.checked = !item.checked;
+            console.log("item.checked: " +  item.checked + "id: " + item.id + " username: " + item.username);
+            item.checked ? this.pushUsername(item.username, item.id, item.checked) : this.popUsername(item.id);
+          } 
+      });
+
+      this.data.filter((item) => item.checked == true ).length == 0 ? this.isCheckInputMessage =  false : this.isCheckInputMessage = true;
+  }
+
+  pushUsername(username: string, id: number, checked: boolean) {
+      this.obj_usersSelected.push({ id: id, username: username, checked: checked });
+  }
+
+  popUsername( id: number ){
+      console.log("POPusername: " + id);
+      const filtrados = this.obj_usersSelected.filter(item => item.id !== id);
+      this.obj_usersSelected = [...filtrados];
+  }
+
+  pop(id: number) {
+    const filtrados = this.obj_usersSelected.filter(item => item.id !== id);
+    this.obj_usersSelected = [...filtrados];
+    this.data.map( item => { 
+        if ( item.id == id ) { 
+          item.checked = !item.checked;
+        } 
+    });
+    this.data.filter((item) => item.checked == true ).length == 0 ? this.isCheckInputMessage =  false : this.isCheckInputMessage = true;
   }
 }
